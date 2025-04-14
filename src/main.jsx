4fc5a6e25b48e6fac4b326847723b3032ce759cd
Thomas Carlier <carlierthomas1@hotmail.fr>
2025-04-14 18:44:53 +0200

    verif

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom' // Changed to HashRouter
-import Home from './pages/Home/'
-import Apropos from './pages/Apropos/'
-import Header from './components/Header'
-import Error from './components/Error'
-import Footer from './components/Footer'
-import FicheLogement from './pages/ficheLogement/'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Import HashRouter
+import Home from './pages/Home/';
+import Apropos from './pages/Apropos/';
+import Header from './components/Header';
+import Error from './components/Error';
+import Footer from './components/Footer';
+import FicheLogement from './pages/ficheLogement/';
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      {' '}
-      {/* Now using HashRouter */}
+    <Router> {/* Utilisation de HashRouter */}
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> {/* Route pour la page d'accueil */}
         <Route path="/Apropos" element={<Apropos />} />
         <Route path="/fiche_logement/:id" element={<FicheLogement />} />
-        <Route path="*" element={<Error />} />
+        <Route path="*" element={<Error />} /> {/* Route pour la page 404 */}
       </Routes>
       <Footer />
     </Router>
   </React.StrictMode>,
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
   },
-  // base: '/P7-Kasa/', // Utilisation du nom de votre dépôt
+  base: '/P7-Kasa/', // **Réactivez cette ligne**
   build: {
     outDir: 'docs',
   },
