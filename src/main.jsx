e12eaaed9304974adde8e1931cc3f4ee23ed54fe
Thomas Carlier <carlierthomas1@hotmail.fr>
2025-04-14 18:15:18 +0200

    Use HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client' // Import correct
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom' // Changed to HashRouter
 import Home from './pages/Home/'
 import Apropos from './pages/Apropos/'
-
 import Header from './components/Header'
 import Error from './components/Error'
 import Footer from './components/Footer'
 import FicheLogement from './pages/ficheLogement/'
 
 const container = document.getElementById('root')
-const root = createRoot(container) // Utilisation de createRoot
+const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
     <Router>
+      {' '}
+      {/* Now using HashRouter */}
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
